076ac5be34b96ca5f38d4ee8292aa9183dacf47b
Dmitry Lenchevsky <49116427+DimitryLenchevsky@users.noreply.github.com>
2026-08-12 08:53:43 +0200

    post and commets are implemented

--- query/index.js
+++ query/index.js
@@ -1,34 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const posts = {}
+const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
 
-app.post('/events', (req, res) => {
-    const { type, data } = req.body;
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
 
-    if (type === 'PostCreated') {
-        const { id, title } = req.body;
-        posts[id] = { id, title , comments: []};
-    }
+  if (type === "PostCreated") {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
 
-    if (type === 'CommentCreated') {
-        const { id, content, postId } = req.body;
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
 
-        const post = posts[postId];
+    const post = posts[postId];
 
-        post.comments.push({ id, content });
-    }
+    post.comments.push({ id, content });
+  }
 
-    res.send({});
+  res.send({});
 });
 
 app.listen(4002, () => {
