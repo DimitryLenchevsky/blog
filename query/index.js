b304a52b8af9c546e93f462c70493e5fb6120a93
Dmitry Lenchevsky <49116427+DimitryLenchevsky@users.noreply.github.com>
2026-08-12 12:48:40 +0200

    completed-app

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -12,10 +13,8 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
-  if (type === "PostCreated") {
+const handleEvent = (type, data) => {
+    if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
   }
@@ -39,10 +38,22 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("Server is running and listening on port 4002");
+
+  const res = await axios.get('http://localhost:4005/events');
+  for (let event of res.data) {
+    console.log('Handling the event: ', event.type);
+    handleEvent(event.type, event.data);
+  }
 });
