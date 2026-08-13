4e0393d91e23a0ce8766dad0ea71fa778a61b518
Dmitry Lenchevsky <49116427+DimitryLenchevsky@users.noreply.github.com>
2026-08-13 12:38:19 +0200

    lint-adjustemnts

--- posts/index.js
+++ posts/index.js
@@ -32,8 +32,8 @@ app.post("/posts", async (req, res) => {
   res.status(201).send(posts[id]);
 });
 
-app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type);
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
 
   res.send({});
 });
