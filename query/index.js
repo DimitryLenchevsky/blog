15f4160fc52d31d87223d0922c90a8c2fa8afd38
Dmitry Lenchevsky <49116427+DimitryLenchevsky@users.noreply.github.com>
2026-08-17 16:08:45 +0200

    all-services-to-kubernetes

--- query/index.js
+++ query/index.js
@@ -51,7 +51,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Server is running and listening on port 4002");
 
-  const res = await axios.get("http://localhost:4005/events");
+  const res = await axios.get("http://event-bus-srv:4005/events");
   for (let event of res.data) {
     console.log("Handling the event: ", event.type);
     handleEvent(event.type, event.data);
